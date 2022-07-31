1cd800d78670c91f07a63909727ee8c72a7ad09a
Eukolos <eminaksoy35@gmail.com>
2022-07-31 16:47:08 +0300

    0.1 dependencies injected

---
+++ src/main/java/com/eukolos/arcadeblog/ArcadeBlogApplication.java
@@ -0,0 +1,13 @@
+package com.eukolos.arcadeblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArcadeBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArcadeBlogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/eukolos/arcadeblog/ArcadeBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eukolos.arcadeblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArcadeBlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
